2702e26cedd7affd90122bf1bdf2aa5273b755c2
TuanThanhTT <tuanthanh1810547@gmail.com>
2025-02-12 09:08:43 +0700

    add new controller GiangVienController

---
+++ WebsiteQuanLyKeKhaiGiangDayCuaGiangVien/Controllers/GiangVienController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteQuanLyKeKhaiGiangDayCuaGiangVien.Controllers
+{
+    public class GiangVienController : Controller
+    {
+        // GET: GiangVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
